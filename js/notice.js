ffaebc4fe55135bb4e334ed75f24aaab341b7624
Heena Saleem <hinasaleem14@gmail.com>
2018-01-08 09:12:41 +0530

    css html and adv css html

---
+++ js/notice.js
@@ -0,0 +1,12 @@
+//if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="noticw.css">');
+
+alert('inside js!!!');
+// $('.notice-close').on('click', function(event){
+//   $('.notice-warning').remove();
+// });
+ function go(){
+ 	alert('inside go function');
+ }
+ // function call
+ go();
+ function 
